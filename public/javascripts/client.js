b3d2b6ec29e11cae803a10d305f5666153d6009e
mizuman <YoshikiMiz@gmail.com>
2015-02-18 17:31:40 +0900

    add text stream

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,19 +1,20 @@
 $(function() {
     var socket = io.connect(null, { port: port });
+    var msg_counter = 0;
+    var msg_list = [];
+    var stream_list = [];
+    var lane_list = [];
 
     socket.on('connect', function() {
         console.log('connect');
     });
 
     socket.on('message', function(msg) {
-        // var date = new Date();
-        // $('#list').prepend($('<dt>' + date + '</dt><dd>' + msg + '</dd>'));
-        $('#list').prepend(msg + '<br>');
-
+        msgStream({text: msg});
     });
 
     socket.on('twitter', function(data) {
-        $('#list').prepend(data.text + '<br>');
+        msgStream(data);
     });
 
     socket.on('disconnect', function() {
@@ -27,4 +28,59 @@ $(function() {
         // message.attr('value', '');
         return false;
     });
+
+    var msgStream = function(data){
+        msg_counter++;
+        if(msg_counter>1000) msg_counter=0;
+        msg_list.push(msg_counter);
+
+        var lane = laneManager(msg_counter ,data.text.length);
+
+        var item = $('<div>')
+                    .attr({
+                        'class': 'tweet tweet' + msg_counter
+                    })
+                    .css('bottom', lane)
+                    .append(data.text);
+
+        $('#list').prepend(item);
+
+        setTimeout(function(){
+            var i = msg_list.splice(0, 1);
+            stream_list.push(i);
+            $(".tweet"+i).addClass('stream')
+        }, 100)
+        setTimeout(function(){
+            var i = stream_list.splice(0,1);
+            $(".tweet"+i).remove();
+        }, 25000)
+    }
+
+    var laneManager = function(num, msg_length){
+        // console.log("length : "+ msg_length)
+        // var lane = num%4*20;
+        var undefined;
+
+        for (var i = 0; i <= lane_list.length; i++) {
+            if(lane_list[i]==undefined){
+                lane_list[i]=num;
+                // console.log(lane_list);
+                break;
+            }
+        //     // console.log("i :"+lane_list[i]);
+
+        };
+
+        // // console.log(i);
+        // console.log("time :", msg_length*60);
+
+        setTimeout(function(){
+            // console.log("i,num:"+i+","+num+","+msg_length*40);
+            lane_list[i]=undefined;
+            // console.log(lane_list);
+        }, msg_length*60)
+
+        var lane = i*20+30;
+        return lane;
+    }
 });
